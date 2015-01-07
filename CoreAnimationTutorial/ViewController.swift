073a74d047de6ede40c1ec67ac5936d32e069e3e
Jameson Quave <jquave@gmail.com>
2015-01-06 18:09:27 -0600

    Part1

--- CoreAnimationTutorial/ViewController.swift
+++ CoreAnimationTutorial/ViewController.swift
@@ -12,14 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setup()
     }
+    
+    func setup() {
+        let redLayer = CALayer()
+        
+        redLayer.frame = CGRect(x: 50, y: 50, width: 50, height: 50)
+        redLayer.backgroundColor = UIColor.redColor().CGColor
+        
+        // Round corners
+        redLayer.cornerRadius = 25
+        
+        // Set border
+        redLayer.borderColor = UIColor.blackColor().CGColor
+        redLayer.borderWidth = 10
+        
+        redLayer.shadowColor = UIColor.blackColor().CGColor
+        redLayer.shadowOpacity = 0.8
+        redLayer.shadowOffset = CGSizeMake(2, 2)
+        redLayer.shadowRadius = 3
+        
+        self.view.layer.addSublayer(redLayer)
+        
+        
+        let imageLayer = CALayer()
+        let image = UIImage(named: "ButterflySmall.jpg")!
+        imageLayer.contents = image.CGImage
+        
+        imageLayer.frame = CGRect(x: 0, y: 100, width: image.size.width, height: image.size.height)
+        imageLayer.contentsGravity = kCAGravityResizeAspect
+        imageLayer.contentsScale = UIScreen.mainScreen().scale
+        self.view.layer.addSublayer(imageLayer)
+        
+        
+        // Create a blank animation using the keyPath "cornerRadius", the property we want to animate
+        let animation = CABasicAnimation(keyPath: "shadowRadius")
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Set the starting value
+        animation.fromValue = redLayer.cornerRadius
+
+        // Set the completion value
+        animation.toValue = 0
+
+        // How may times should the animation repeat?
+        animation.repeatCount = 1000
+
+        // Finally, add the animation to the layer
+        redLayer.addAnimation(animation, forKey: "cornerRadius")
+        
+        
     }
-
 
 }
 
